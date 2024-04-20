a88d4c2fd5170cd8d009b0ce8d7bf3a03d400775
Yue Liu <quic_yueliu@quicinc.com>
2024-04-20 12:32:53 +0530

    sm6475-common: Enable parallel module loading for first stage
    Add androidboot.load_modules_parallel=true to bootconfig to
    enable prallel module loading for first stage.
    
    Change-Id: I47e3eef44a652feb428d0b372239b14c5d1f816b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,6 +81,7 @@ BOARD_KERNEL_CMDLINE += \
     printk.devkmsg=on
 BOARD_BOOTCONFIG += \
     androidboot.hardware=qcom \
+    androidboot.load_modules_parallel=true \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_IMAGE_NAME := Image
