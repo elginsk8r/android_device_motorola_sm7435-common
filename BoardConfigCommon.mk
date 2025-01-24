9936b507c2307d1f3013a3b77806aeacce1d085d
Mukul Nayak <quic_mukunaya@quicinc.com>
2025-01-24 12:01:06 +0530

    sm6475-common: Enable AVF for parrot
    Change-Id: Ic31effe64d0ecc5942b09a658ef022538af5b3db

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,6 +82,7 @@ BOARD_KERNEL_CMDLINE += \
     printk.devkmsg=on
 BOARD_BOOTCONFIG += \
     androidboot.hardware=qcom \
+    androidboot.hypervisor.protected_vm.supported=true \
     androidboot.load_modules_parallel=true \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3
