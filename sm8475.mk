4728562a44b20079395442c79e703847d30339d5
Michael Bestas <mkbestas@lineageos.org>
2025-03-14 02:30:39 +0200

    sm6375-common: Inherit common motorola FCM
    Change-Id: Ie7bfd47c8727ebdc961ab31c8cf1b71803a5eff3

--- sm8475.mk
+++ sm8475.mk
@@ -397,7 +397,7 @@ PRODUCT_COPY_FILES += \
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    device/motorola/sm8475-common/device_framework_matrix.xml \
+    hardware/motorola/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/motorola/sm8475-common/framework_manifest.xml
