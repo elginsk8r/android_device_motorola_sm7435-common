aee86b3b046d572002e18881f806a56500cf881d
dianlujitao <dianlujitao@lineageos.org>
2025-03-20 22:04:37 +0800

    sm7435-common: Retain old audio AIDL version
    Simply replacing the AIDL interface with a different version leads to
    segmentation faults.
    
    Change-Id: Iad5fd10592ec76c7daf6f4f133a7e6b1e773f3de

--- common.mk
+++ common.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libalsautilsv2.vendor \
     libbatterylistener \
     lib_bt_aptx \
     lib_bt_ble \
