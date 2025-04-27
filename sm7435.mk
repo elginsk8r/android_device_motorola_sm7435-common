7a4755b8d933c46d5c25874121c301db0f8f996b
Bruno Martins <bgcngm@gmail.com>
2025-04-27 22:42:58 +0100

    sm8475-common: Move to moto_usb_compositions.conf
    Change-Id: I3e8be30f3cc901554de74fda239f3cd1e1a671c5

--- sm7435.mk
+++ sm7435.mk
@@ -371,7 +371,7 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget-service.qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
-    usb_compositions.conf
+    moto_usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
