570af8861e9253e833c4d73df73fb7cf74dbcec9
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-06-29 22:00:44 +0300

    sm7435-common: Apply changes from avatrn_g-user 15 V1UANS35H.98-24-1
    Change-Id: Ie05e6d0e822d2bde756a2fed5c7f8e917c79ca2a

--- extract-files.py
+++ extract-files.py
@@ -70,14 +70,6 @@ blob_fixups: blob_fixups_user_type = {
         'android.hardware.security.sharedsecret-V1-ndk.so',
     )
     .add_needed('android.hardware.security.rkp-V1-ndk.so'),
-    (
-        'vendor/bin/init.kernel.post_boot-parrot.sh',
-        'vendor/bin/init.kernel.post_boot-ravelin.sh',
-    ): blob_fixup().regex_replace('ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'),
-    'vendor/bin/qcc-trd': blob_fixup().replace_needed(
-        'libgrpc++_unsecure.so', 'libgrpc++_unsecure_prebuilt.so'
-    ),
-    'vendor/lib64/libmotext_inf.so': blob_fixup().remove_needed('libril.so'),
     'system_ext/priv-app/ims/ims.apk': blob_fixup().apktool_patch(
         'ims-patches'
     ),
@@ -89,10 +81,11 @@ blob_fixups: blob_fixups_user_type = {
     ),
     'vendor/lib64/sensors.moto.so': blob_fixup().add_needed('libbase_shim.so'),
     (
-        'vendor/etc/seccomp_policy/modemManager.policy',
-        'vendor/etc/seccomp_policy/sensors-qesdk.policy',
-    ): blob_fixup().add_line_if_missing('gettid: 1'),
-    'vendor/etc/media_codecs_cape.xml': blob_fixup().regex_replace(
+        'vendor/etc/media_codecs_parrot_v0.xml',
+        'vendor/etc/media_codecs_parrot_v1.xml',
+        'vendor/etc/media_codecs_parrot_v2.xml',
+        'vendor/etc/media_codecs_ravelin.xml'
+    ): blob_fixup().regex_replace(
         '.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''
     ),
 }
