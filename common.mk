e9afa48e325829e44f83513e4da2a9dd1cf4eaf5
Nick Reuter <nreuter85@gmail.com>
2026-05-14 10:45:21 -0500

    sm6475-common: Update qcom scripts from W1UQ36H.73-6
    Change-Id: I38aeb496c20a493997e3d334cb97b0557b90eb9c

--- common.mk
+++ common.mk
@@ -172,6 +172,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.target.rc \
+    ueventd.odm.rc \
     ueventd.qcom.rc \
     init.class_main.sh \
     init.mmi.boot.sh \
