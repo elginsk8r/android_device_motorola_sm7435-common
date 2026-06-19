deef50686f4cf9193eed50dae8ae56471c2a64a1
Nick Reuter <nreuter85@gmail.com>
2026-06-18 23:22:31 -0500

    sm6475-common: Update device manifest and matrix
    Change-Id: I274e5b4f4f2636338c22d073f9a35359064d84bb

--- common.mk
+++ common.mk
@@ -387,11 +387,11 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VINTF
-DEVICE_FRAMEWORK_MANIFEST_FILE += device/motorola/sm6475-common/vintf/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/motorola/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix_aidl.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix_aidl.xml \
+    device/motorola/sm6475-common/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += device/motorola/sm6475-common/vintf/manifest.xml
 
 # Virtualization
